90a1d54764e152996a203cf93f9bf3faf2e65f04
lukwiya23 <luk23bonnie8@gmail.com>
2021-06-28 16:41:35 +0300

    ||Remastered Ui||

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-const navMenu = document.getElementById('nav-menu')
-const navToggle = document.getElementById('nav-toggle')
-const navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if(navToggle){
-    navToggle.addEventListener('click',()=>{
+    navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
     })
 }
@@ -14,7 +15,7 @@ if(navToggle){
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 if(navClose){
-    navClose.addEventListener('click', ()=>{
+    navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
 }
@@ -29,77 +30,100 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-
 /*==================== ACCORDION SKILLS ====================*/
-const skillsContent = document.getElementsByClassName('skills__content')
-const skillsHeader = document.querySelectorAll('.skills__header')
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
 
 function toggleSkills(){
-    let itemClass = this.parentNode.ClassName
-
+    let itemClass = this.parentNode.className
+    
     for(i = 0; i < skillsContent.length; i++){
         skillsContent[i].className = 'skills__content skills__close'
-
     }
-
     if(itemClass === 'skills__content skills__close'){
         this.parentNode.className = 'skills__content skills__open'
     }
 }
 
-skillsHeader.forEach((el)=>{
+skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
 })
-/*==================== QUALIFICATION TABS ====================*/
 
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+        
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+        
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 const modalViews = document.querySelectorAll('.services__modal'),
-       modalBtns = document.querySelectorAll('.services__button') ,
-       modalCloses = document.querySelectorAll('.services__modal-close')
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
 
 let modal = function(modalClick){
     modalViews[modalClick].classList.add('active-modal')
 }
 
-modalBtns.forEach((modalBtn, i) =>{
-    modalBtn.addEventListener('click', ()=>{
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
         modal(i)
     })
 })
 
-modalCloses.forEach((modalClose) =>{
-    modalClose.addEventListener('click', ()=>{
-        modalViews.forEach((modalView) =>{
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
             modalView.classList.remove('active-modal')
         })
     })
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper('.swiper-container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
     cssMode: true,
-    loop:true,
+    loop: true,
+
     navigation: {
-        nextEl: 'swiper-button-next',
-        prevEl: 'swiper-button-prev',
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
-    pagintion:{
-        el: '.swiper-pagination',
-        dynamicBullets:true,
-        clickable:true
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
     },
- 
-    mousewheel: true,
-    keyboard: true,
-    breakpoints:{
-        568:{
-            slidesPerView:2
-        }
-    }
 })
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        },
+    }
+})
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
@@ -121,27 +145,23 @@ function scrollActive(){
     })
 }
 window.addEventListener('scroll', scrollActive)
+
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
 function scrollHeader(){
-    const nav = document.getElementById('header')
-    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+    const nav = document.getElementById('header');
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader)
 
-
-/*==================== SHOW SCROLL UP ====================*/ 
+/*==================== SHOW SCROLL TOP ====================*/ 
 function scrollTop(){
-    const scrollTop = document.getElementById('scroll-up');
+    let scrollTop = document.getElementById('scroll-top');
     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+    if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
 
-/*=
-
 /*==================== DARK LIGHT THEME ====================*/ 
-
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'uil-sun'
